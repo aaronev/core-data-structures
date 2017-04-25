df3b18e508056bb810bf3ca5683066ef66228450
Stephani McGrath <silentsoundz1977@gmail.com>
2017-04-25 13:41:13 -0700

    implementation for stack is done: all tests passing

--- spec/stack.js
+++ spec/stack.js
@@ -4,7 +4,7 @@ import Stack from '../src/stack'
 
 chai.use(chaiChange)
 
-describe('Stack', () => {
+describe.only('Stack', () => {
   'use strict'
 
   it('exists', () => {
@@ -14,9 +14,45 @@ describe('Stack', () => {
   context('push()', () => {
     it('pushes an element to the top of the stack.', () => {
       const myStack = new Stack()
+      expect(myStack.push('foo')).to.equal(myStack.top)
+    })
+  })
 
-      expect(() => myStack.push('foo'))
-        .to.alter(() => myStack.length(), { from: 0, to: 1 })
+  context('pop()', () => {
+    it('returns and removes the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      myStack.push('fi')
+      myStack.push('foo');
+      expect(myStack.pop('foo')).to.equal(myStack.top)
+    })
+  })
+
+  context('peek()', () => {
+    it('returns the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      myStack.push('fi')
+      myStack.push('foo');
+      expect(myStack.peek()).to.equal(myStack.top)
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('returns the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      expect(myStack.isEmpty()).to.equal(true)
+      myStack.push('fi')
+      myStack.push('foo');
+      expect(myStack.isEmpty()).to.equal(false)
+    })
+  })
+
+  context('length()', () => {
+    it('returns the number of elements in the stack.', () => {
+      const myStack = new Stack()
+      expect(myStack.length()).to.equal(0)
+      myStack.push('fi')
+      myStack.push('foo');
+      expect(myStack.length()).to.equal(2)
     })
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,37 @@
+import Node from './node'
 'use strict'
 
 export default class Stack {
-  // your code here
-}
+  constructor() {
+    this.top = null
+    this.bottom = null
+    this.size = 0
+  }
+
+  push(data) {
+    let node = new Node(data)
+    if (this.top !== null) {
+      node.setNext(this.top)
+      this.top = node
+    }
+    else { this.top = node }
+    this.size ++
+    return this.top
+  }
+
+  pop(data) {
+  let node = new Node(data)
+  if (this.top !== null) {
+    this.top = this.top.next
+  }
+  this.size --
+  return this.top
+  }
+
+  peek(){return this.top}
+
+  isEmpty() { return this.size === 0 ? true : false }
+
+  length() { return this.size  }
+
+  }
